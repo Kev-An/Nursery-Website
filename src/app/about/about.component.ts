668888cf2de8e0ac073ea8dc0813f9ce6acc0050
KevAn647 <kevin.anthony@cohesivehealth.in>
2026-07-08 13:51:06 +0530

    Fixed the story image and removed navbar

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -19,7 +19,7 @@ export class AboutComponent {
     { label: 'Contact', href: '#contact', active: false }
   ];
 
-  storyImageUrl = 'https://unsplash.com/photos/green-potted-plants-on-brown-wooden-seat-dDVU6D_6T80';
+  storyImageUrl = 'https://images.unsplash.com/photo-1601985705806-5b9a71f6004f?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
   imageBadge = {
     value: '12+',
