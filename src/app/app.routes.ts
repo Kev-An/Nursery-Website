97763ce260c210096ef18bdaf8f19bb34d4119be
KevAn647 <kevin.anthony@cohesivehealth.in>
2026-07-17 13:34:34 +0530

    Removed promo code, removed wishlist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { CareComponent } from './care/care.component';
 import { AboutComponent } from './about/about.component';
 import { CartComponent } from './cart/cart.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin/admin.component';
@@ -15,7 +14,6 @@ export const routes: Routes = [
   { path: 'care', component: CareComponent },
   { path: 'about', component: AboutComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'wishlist', component: WishlistComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminDashboardComponent },
